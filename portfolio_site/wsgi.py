f4b48c00e9c918711db1b938f6231b8628ecdeda
harshmangal1 <143318682+harshmangal1@users.noreply.github.com>
2026-03-11 21:42:44 +0530

    Create superuser at startup

--- portfolio_site/wsgi.py
+++ portfolio_site/wsgi.py
@@ -4,3 +4,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_site.settings')
 
 application = get_wsgi_application()
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin2').exists():
+    User.objects.create_superuser('admin2', 'admin2@example.com', 'admin123456')
