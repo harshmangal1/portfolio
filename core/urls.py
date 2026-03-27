d4c6d96b4d0fb92b694b8dc9042a65e837be45a8
harshmangal1 <143318682+harshmangal1@users.noreply.github.com>
2026-03-27 17:40:05 +0530

    Add temporary admin password reset URL

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,16 @@
 from django.urls import path
+from django.http import HttpResponse
 from . import views
+
+def reset_admin_password(request):
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    user, created = User.objects.get_or_create(username='admin')
+    user.set_password('Admin@123')
+    user.is_staff = True
+    user.is_superuser = True
+    user.save()
+    return HttpResponse(f"Admin password reset! Username: admin, Password: Admin@123<br><a href='/admin/'>Go to Admin</a>")
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -10,4 +21,5 @@ urlpatterns = [
     path('blog/', views.blog_index, name='blog_index'),
     path('blog/<slug:slug>/', views.blog_detail, name='blog_detail'),
     path('contact/', views.contact, name='contact'),
+    path('reset-admin/', reset_admin_password),
 ]
