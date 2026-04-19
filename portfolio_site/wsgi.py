146b0cd4d6ad7774390c99c6a399eaad574fbdcd
harshmangal1 <143318682+harshmangal1@users.noreply.github.com>
2026-04-19 23:58:48 +0530

    Auto-run migrations on Render startup

--- portfolio_site/wsgi.py
+++ portfolio_site/wsgi.py
@@ -1,6 +1,10 @@
 import os
 from django.core.wsgi import get_wsgi_application
+from django.core.management import execute_from_command_line
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_site.settings')
 
 application = get_wsgi_application()
+
+if os.environ.get('RENDER') == 'true':
+    execute_from_command_line(['manage.py', 'migrate', '--noinput'])
